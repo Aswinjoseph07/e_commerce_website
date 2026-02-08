a66e4bc55025fdf452100d97f81132ec4f50e1d5
Aswinjoseph07 <aswinjoseph9361@gmail.com>
2026-02-08 16:15:02 +0530

    final commit

--- backend/src/data/products.js
+++ backend/src/data/products.js
@@ -16,7 +16,37 @@ const products = [
     name: "Silver Bracelet",
     price: 15000,
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgQ99Ap3Db5gfQIgbEfDnJh2q0Xm9BYxcPCg&s"
-  }
+  },
+  {
+    id: 4,
+    name: "Gold Earings",
+    price: 18000,
+    image: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcR1srEdXLeFHxXWZeosjmXsqbiQrJm9j7ATMi-Qs5rGd-p0T8PWxSQTiSpv2s6i9YxyWehuT-JMCcaHoe8QNRI7OTdf-mbRzYWw-anlEaxA9gOaGU01iKTNxc8nDVvBIikvx09toA&usqp=CAc"
+  },{
+    id: 5,
+    name: "Gold ring",
+    price: 8000,
+    image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQWqvFY-Zoo6OFXKqoWiZ8PkafhYeXQ2ck4KrxnhfA5JqmuR0Is5IFSpZfFF7aimDUdKkPiw6ZfTaqMNgV24g4jajLgRE3nQPBjevmafoQgPx7YxxgmlgRfQzoeBXHhk5V1vogrUE5J&usqp=CAc"
+  },
+  {
+    id: 6,
+    name: "Gold Bracelet",
+    price: 45000,
+    image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRYK2EhVk740t6pqaHjJgDh8N0589rpZSGozSw8kHX92SOa1WhV03GszJzyNt3nihfyw3OlhzvvsaHFQZ3WtcDao_JrJktGUPFMk11FWpGu6prnOW2zZPhYAAsfc1s8Mw1Zw42d8A&usqp=CAc"
+  },
+   {
+    id: 7,
+    name: "Gold Bangle",
+    price: 65000,
+    image: "https://d25g9z9s77rn4i.cloudfront.net/uploads/product/1271/1680274623_966c06dc8d2152bb195a.jpg"
+  },
+   {
+    id: 8,
+    name: "platinam chain",
+    price: 45000,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMoZWILdLiOE19Ubn7NHbOPDk6EyCr8Y3W7g&s"
+  },
+
 ]
 
 module.exports = products
